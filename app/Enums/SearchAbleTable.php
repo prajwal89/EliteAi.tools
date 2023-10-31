1d55ddce68ddbbc6c869bf58591027fbe25d8e0c
prajwal89 <prajwalhallale89@gmail.com>
2023-10-31 13:33:02 +0530

    documentation

--- app/Enums/SearchAbleTable.php
+++ app/Enums/SearchAbleTable.php
@@ -3,23 +3,36 @@
 namespace App\Enums;
 
 use App\Models\Tool;
+use Exception;
 use Illuminate\Database\Eloquent\Model;
 
 enum SearchAbleTable: string
 {
-    case TOOL = 'tool';
+    case TOOL = 'tool';  //table name
 
+    /**
+     * index name is {prefix}_{tableName}
+     *
+     * @return string
+     */
     public function getIndexName(): string
     {
         return match ($this) {
-            SearchAbleTable::TOOL => config('custom.meilisearch.prefix') . '_tools',
+            SearchAbleTable::TOOL => config('custom.meilisearch.prefix') . '_' . $this->getModelInstance()->getTable(),
+            default => throw new Exception("Cannot get index name for table {$this->value}"),
         };
     }
 
+    /**
+     * For sending data for indexing
+     *
+     * @return string
+     */
     public function getBatchSize(): string
     {
         return match ($this) {
             SearchAbleTable::TOOL => 5000,
+            default => throw new Exception("Cannot get batch size for table {$this->value}"),
         };
     }
 
@@ -27,6 +40,7 @@ enum SearchAbleTable: string
     {
         return match ($this) {
             SearchAbleTable::TOOL => new Tool(),
+            default => throw new Exception("Cannot get model instance for table {$this->value}"),
         };
     }
 
