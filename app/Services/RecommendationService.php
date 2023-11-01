4a09c9d0d3f7110f21566cf599de5d6dfbb59a3f
prajwal89 <prajwalhallale89@gmail.com>
2023-11-01 11:15:54 +0530

    RecommendationService

---
+++ app/Services/RecommendationService.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Services;
+
+ini_set('memory_limit', '2048M');
+ini_set('max_execution_time', 600); //10 min
+
+use App\Enums\SearchAbleTable;
+use App\Models\SemanticScore;
+use App\Models\Tool;
+use App\ValueObjects\SemanticScore as ValueObjectsSemanticScore;
+use Illuminate\Support\Collection;
+
+class RecommendationService
+{
+    /**
+     * Save distance of $toolId relative to all other tools
+     *
+     * @return void
+     */
+    public static function saveSemanticScoresFor(int $toolId, int $toolsLimit = 500)
+    {
+        $tool = Tool::find($toolId);
+
+        $results = MeilisearchService::vectorSearch(
+            SearchAbleTable::TOOL,
+            $tool->getParagraphForVectorEmbeddings(),
+            ['limit' => $toolsLimit]
+        );
+
+        foreach ($results['hits'] as $hit) {
+            // *same tool this will always result in 1.00000000
+            if ($tool->id == $hit['id']) {
+                continue;
+            }
+
+            SemanticScore::updateOrCreate([
+                'tool1_id' => min($tool->id, $hit['id']),
+                'tool2_id' => max($tool->id, $hit['id']),
+            ], [
+                'score' => $hit['_semanticScore'],
+            ]);
+        }
+    }
+
+    /**
+     * hint for scrutiny: count of all possible combinations of pairs of numbers from e.g, 1,39 
+     * and do not count pair having same digits
+     *
+     * @param integer $forToolId
+     * @param float $score
+     * @param integer $maxTools
+     * @return Collection
+     */
+    public static function baseOnSemanticScores(
+        int $forToolId,
+        float $score = 0.5,
+        int $maxTools = 500
+    ): Collection {
+
+        $semanticScore = new ValueObjectsSemanticScore($score);
+
+        $scores = SemanticScore::where(function ($query) use ($forToolId) {
+            $query->where('tool1_id', $forToolId);
+            $query->orWhere('tool2_id', $forToolId);
+        })
+            ->where('score', '>', $semanticScore->getScore())
+            ->orderBy('score', 'desc')
+            ->limit($maxTools)
+            ->get();
+
+        $toolIds = $scores->map(function ($score) {
+            return [$score->tool1_id, $score->tool2_id];
+        })
+            ->flatten()
+            ->filter(function ($tooId) use ($forToolId) {
+                //* remove current tool that we are getting alternatives for
+                return $tooId !== $forToolId;
+            })
+            ->unique();
+
+        $resultTools = Tool::with(['categories'])
+            ->whereIn('id', $toolIds->toArray())
+            ->get()
+            ->sortBy(function ($tool) use ($toolIds) {
+                return array_search($tool->id, $toolIds->toArray());
+            });
+
+        // dump($scores->toArray());
+        // dump($toolIds->toArray());
+        // dump($resultTools->toArray());
+
+        return $resultTools;
+    }
+}
---
+++ app/ValueObjects/SemanticScore.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\ValueObjects;
+
+use InvalidArgumentException;
+
+class SemanticScore
+{
+    private $score;
+
+    public function __construct(float $score)
+    {
+        if ($score >= 0.0 && $score <= 1.0) {
+            $this->score = $score;
+        } else {
+            throw new InvalidArgumentException("Semantic score must be between 0.0 and 1.0");
+        }
+    }
+
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    public function compare(SemanticScore $otherScore)
+    {
+        return $this->score - $otherScore->getScore();
+    }
+}
