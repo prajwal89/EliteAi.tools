a427ebe3dd858216d4f223daf8857c5f04148f43
prajwal89 <prajwalhallale89@gmail.com>
2023-11-03 17:02:13 +0530

    show tags

--- resources/views/partials/tools/tool-details.blade.php
+++ resources/views/partials/tools/tool-details.blade.php
@@ -34,25 +34,50 @@
                  <a class="text-primary-600" href="{{ $tool->home_page_url }}">{{ $tool->home_page_url }}</a>
              </p>
 
-             <div class="flex items-center gap-2 py-2">
-                 <span>Categories:</span>
-                 <ul class="flex flex-wrap text-sm gap-2">
-                     @foreach ($tool->categories as $category)
-                         <li>
-                             <a class="flex items-center gap-2 hover:text-primary-600"
-                                 href="{{ route('category.show', ['category' => $category->slug]) }}">
+             <div class="my-2">
+                 <div class="flex items-center gap-2 my-2">
+                     <span>Categories:</span>
+                     <ul class="flex flex-wrap gap-2">
+                         @foreach ($tool->categories as $category)
+                             <li>
+                                 <a class="flex items-center gap-2 hover:text-primary-600"
+                                     href="{{ route('category.show', ['category' => $category->slug]) }}">
 
-                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                                     <path stroke-linecap="round" stroke-linejoin="round"
-                                         d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
-                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
-                                 </svg>
-                                 <span>{{ $category->name }}</span>
-                             </a>
-                         </li>
-                     @endforeach
-                 </ul>
+                                     {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                                    </svg> --}}
+                                     <span>{{ $category->name }} {{ $loop->last ? '' : ',' }}</span>
+                                 </a>
+                             </li>
+                         @endforeach
+                     </ul>
+                 </div>
+
+                 <div class="flex gap-2 mt-4">
+                     <span>
+                         Tags:
+                     </span>
+                     <ul class="flex flex-wrap gap-x-2">
+                         @foreach ($tool->tags as $tag)
+                             <li>
+                                 <a class="flex items-center gap-2 hover:text-primary-600" href="">
+                                     {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                         <path stroke-linecap="round" stroke-linejoin="round"
+                                             d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6z" />
+                                     </svg> --}}
+                                     <span>{{ $tag->name }} {{ $loop->last ? '' : ',' }}</span>
+
+                                 </a>
+                             </li>
+                         @endforeach
+                     </ul>
+                 </div>
+
              </div>
 
              @if (
