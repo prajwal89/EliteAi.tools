d4728fe1765fb05ad6f66b19ed7bf52f90a90947
prajwal89 <prajwalhallale89@gmail.com>
2023-11-04 18:03:48 +0530

    exists

--- app/Livewire/ToolImporter.php
+++ app/Livewire/ToolImporter.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\DTOs\ToolDTO;
 use App\DTOs\ToolSocialHandlesDTO;
+use App\Models\Tool;
 use App\Services\ExtractedToolProcessor;
 use App\Services\WebPageFetcher;
 use Exception;
@@ -34,6 +35,10 @@ class ToolImporter extends Component
     public function getData()
     {
         $this->url = rtrim($this->url, '/');
+
+        if (Tool::where('domain_name', getDomainFromUrl($this->url))->exists()) {
+            dd('We already have this tool.');
+        }
 
         $html = (new WebPageFetcher($this->url))->get()->content;
 
@@ -154,55 +159,6 @@ Tool Url: ' . $this->url . '
         return ToolSocialHandlesDTO::fromArray($socialMediaUserHandles);
     }
 
-    // // Function to extract user handles based on platform
-    // public function extractUserHandle($url, $platform)
-    // {
-    //     // return $url;
-
-    //     if ($platform === 'instagram') {
-    //         preg_match('/instagram\.com\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'tiktok') {
-    //         preg_match('/tiktok\.com\/@([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'twitter') {
-    //         preg_match('/twitter\.com\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'linkedin') {
-    //         preg_match('/linkedin\.com\/in\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'linkedin_company') {
-    //         preg_match('/linkedin\.com\/company\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'facebook') {
-    //         preg_match('/facebook\.com\/profile\.php\?id=([^&]+)/', $url, $matches);
-    //     } elseif ($platform === 'youtube_channel') {
-    //         preg_match('/youtube\.com\/channel\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'discord_channel_invite_id') {
-    //         // https://discord.com/invite/Naa2qkyMkt
-    //         preg_match('/discord\.com\/invite\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'subreddit_id') {
-    //         // https://www.reddit.com/r/NSFWCharacterAI/
-    //         preg_match('/www.reddit.com\/r\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'telegram_channel_id') {
-    //         // https://t.me/+J_J28uSPzTM1NWZl
-    //         preg_match('/t.me\/([^\/]+)/', $url, $matches);
-    //     } elseif ($platform === 'email') {
-    //         preg_match('/mailto:(.*)/', $url, $matches);
-    //         if (isset($matches[1])) {
-    //             $handle = str($matches[1])->before('?')->toString();
-    //         }
-    //     } elseif ($platform === 'android_app') {
-    //         // Parse the URL and extract the query string
-    //         $queryString = parse_url($url, PHP_URL_QUERY);
-
-    //         // Parse the query string and extract the 'id' parameter
-    //         parse_str($queryString, $queryParameters);
-
-    //         $handle = isset($queryParameters['id']) ? $queryParameters['id'] : '';
-    //     } elseif ($platform === 'ios_app') {
-    //         if (preg_match('/id(\d+)/', $url, $matches)) {
-    //             $handle = 'id' . $matches[1];
-    //         }
-    //     }
-
-    //     return $handle ?? $matches[1] ?? '';
-    // }
 
     public function extractUserHandle(string $url, string $platform): string
     {
