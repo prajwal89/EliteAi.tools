6959032822077a0abd04f4f35b12af6fd823af8d
Prajwal Hallale <22263436+prajwal89@users.noreply.github.com>
2023-11-09 15:22:00 +0530

    restrict category deletation

---
+++ database/migrations/2023_11_09_093851_restrict_migrateion.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('category_tool', function (Blueprint $table) {
+            // $table->dropForeign('category_tool_category_id_foreign');
+            $table->dropForeign(['category_id']);
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
