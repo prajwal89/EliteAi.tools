8a33d78779bce18a02100b582f39f03f4b9a7856
prajwal89 <prajwalhallale89@gmail.com>
2023-11-21 21:36:03 +0530

    [+] charts

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,18 @@
 @extends('layouts.admin')
+@section('head')
+    @livewireChartsScripts
+@stop
+@section('content')
+    {{-- <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> --}}
+    <div style="height: 20rem;">
+        <livewire:livewire-column-chart :column-chart-model="$columnChartModel" />
+    </div>
+
+    <div class="row">
+        <div class="col-6" style="height: 32rem;">
+
+        </div>
+        <div class="col-6"></div>
+
+    </div>
+@stop
