54fb6b7e792c0ffe3bfb53379309e88e09ca7985
prajwal89 <prajwalhallale89@gmail.com>
2023-11-28 12:34:22 +0530

    privacy policy and toc

--- resources/views/pages/terms-and-conditions.blade.php
+++ resources/views/pages/terms-and-conditions.blade.php
@@ -2,7 +2,41 @@
 @section('title', 'Terms and Conditions')
 
 @section('content')
-    <div class="prose max-w-5xl px-4 lg:px-8 mx-auto my-8">
+    <div class="prose max-w-3xl px-4 lg:px-8 mx-auto my-8">
         <h1>Terms and Conditions</h1>
+        <p><strong>1. Acceptance of Terms</strong></p>
+        <p>By accessing or using the https://eliteai.tools website (the "Website"), you agree to comply with and be bound
+            by these Terms and Conditions.</p>
+        <p><strong>2. Changes to Terms</strong></p>
+        <p>EliteAi.tools reserves the right to change, modify, or revise these Terms and Conditions at any time.
+            Your continued use of the Website after any such changes constitutes your acceptance of the new Terms and
+            Conditions.</p>
+        <p><strong>3. Description of Service</strong></p>
+        <p>The Website provides a platform to list and discover AI tools. EliteAi.tools does not endorse,
+            guarantee, or assume responsibility for any AI tools listed on the Website.</p>
+        <p><strong>4. User Accounts</strong></p>
+        <p>To access certain features of the Website, you may be required to create an account. You are responsible for
+            maintaining the confidentiality of your account and password. EliteAi.tools reserves the right to
+            terminate accounts, remove or edit content, and refuse service in its sole discretion.</p>
+        <p><strong>5. Content Guidelines</strong></p>
+        <p>Users are solely responsible for the content they submit on the Website. Content must not violate any
+            applicable laws or regulations and should not infringe on the intellectual property rights of others.</p>
+        <p><strong>6. Privacy Policy</strong></p>
+        <p>By using the Website, you agree to the terms of our Privacy Policy, which is incorporated into these Terms
+            and Conditions by reference. <a href="{{ route('privacy-policy') }}">Privacy Policy</a></p>
+        <p><strong>7. Intellectual Property</strong></p>
+        <p>All content on the Website, including but not limited to text, graphics, logos, and images, is the property
+            of EliteAi.tools and is protected by copyright, trademark, and other intellectual property laws.</p>
+        <p><strong>8. Limitation of Liability</strong></p>
+        <p>EliteAi.tools is not liable for any direct, indirect, incidental, consequential, or special damages
+            arising out of or in any way connected with the use of the Website.</p>
+        <p><strong>9. Indemnification</strong></p>
+        <p>You agree to indemnify and hold EliteAi.tools harmless from any claims, losses, or damages, including
+            attorney's fees, arising out of your use of the Website or violation of these Terms and Conditions.</p>
+
+        <p><strong>11. Contact Information</strong></p>
+        <p>If you have any questions about these Terms and Conditions, please contact us at admin@eliteai.tools.</p>
+        <p>By using the Website, you acknowledge that you have read, understood, and agree to be bound by these Terms
+            and Conditions.</p>
     </div>
 @stop
