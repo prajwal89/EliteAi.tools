a736185b1402fc4118cbb2f1c8d4275e5c0a1527
prajwal89 <prajwalhallale89@gmail.com>
2023-12-15 17:20:47 +0530

    u

--- deployment-script.sh
+++ deployment-script.sh
@@ -15,7 +15,8 @@ cd domains/eliteai.tools/public_html
 git stash
 git pull
 composer2 install --ignore-platform-reqs --no-scripts
-composer2 dump-autoload
+composer2 dump-autoload --ignore-platform-reqs --no-scripts
+php artisan migrate
 php artisan optimize:clear
 php artisan optimize
 php artisan test
